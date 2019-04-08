910053f915beeb7878cff3e522031c6d4da46a11
CollinMurdock <murdoccr@miamioh.edu>
2019-04-08 17:30:14 -0400

    changed price display

--- session.js
+++ session.js
@@ -51,6 +51,12 @@ class AppItem{
 	element;
 	constructor(app){
 
+		var price;
+		if(app.price != 0)
+			price = "$" + app.price;
+		else
+			price = "Free!"
+
 		this.element = $('<li class="app-item">\
 			<div class="top-block pure-g">\
 				<h1 class="pure-u-1-3 title">'+app.name+'</h1>\
@@ -58,7 +64,7 @@ class AppItem{
 			</div>\
 			<div class="bottom-block pure-g">\
 				<div class="pure-u-1-5 version"><p><b>Version</b></p>'+app.version+'</div>\
-				<div class="pure-u-1-5 price"><p><b>Price:</b></p>'+app.price+'</div>\
+				<div class="pure-u-1-5 price"><p><b>Price:</b></p>'+price+'</div>\
 				<div class="pure-u-1-5 devs"><p><b>Developers</b></p><ul>'+listElements(app.devs)+'</ul></div>\
 				<div class="pure-u-1-5 plats"><p><b>Platforms</b></p><ul>'+listElements(app.platforms)+'</ul></div>\
 				<div class="pure-u-1-5 link"><a href="'+app.link+'">Link</a></div>\
