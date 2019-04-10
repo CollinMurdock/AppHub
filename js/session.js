a98d6f4f3d12e9f2d693745890ce8f3c308f3ff8
CollinMurdock <murdoccr@miamioh.edu>
2019-04-10 10:17:37 -0400

    fixed header issue

--- js/session.js
+++ js/session.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
 	initializeFirebase();
 	var user = sessionStorage.getItem("user");
 	if(user !== null){
-		activeUser = JSON.parse(user);
-		console.log("Logged in! Username: "+activeUser.username);	
+		userLoggedIn(user);
 	}else{
 		console.log("No one logged in");
 	}
@@ -30,6 +29,23 @@ $(document).ready(function(){
 	
 });
 
+function userLoggedIn(user){
+	activeUser = JSON.parse(user);
+	console.log("Logged in! Username: "+activeUser.username);
+	$("#login-button").css("display","none");
+	$("#create-button").css("display","none");
+	$("#signout-button").css("display","inline");
+	$("#proposal-button").css("display","inline");	
+	$("#welcome-user").css("display","block");
+
+}
+
+function signOut(){
+	sessionStorage.removeItem("user");
+	location.reload(true);
+	console.log("signed out");
+}
+
 
 function initializeFirebase() {
   var config = {
@@ -43,4 +59,45 @@ function initializeFirebase() {
   firebase.initializeApp(config);
 
   database = firebase.database();
+}
+
+
+
+class AppItem{
+	element;
+	constructor(app){
+
+		var price;
+		if(app.price != 0)
+			price = "$" + app.price;
+		else
+			price = "Free!"
+
+		this.element = $('<li class="app-item">\
+			<div class="top-block pure-g">\
+				<h1 class="pure-u-1-3 title">'+app.name+'</h1>\
+				<div class="pure-u-2-3 description" ><p>'+app.desc+'</p></div>\
+			</div>\
+			<div class="bottom-block pure-g">\
+				<div class="pure-u-1-5 version"><p><b>Version</b></p>'+app.version+'</div>\
+				<div class="pure-u-1-5 price"><p><b>Price:</b></p>'+price+'</div>\
+				<div class="pure-u-1-5 devs"><p><b>Developers</b></p><ul>'+listElements(app.devs)+'</ul></div>\
+				<div class="pure-u-1-5 plats"><p><b>Platforms</b></p><ul>'+listElements(app.platforms)+'</ul></div>\
+				<div class="pure-u-1-5 link"><a href="'+app.link+'">Link</a></div>\
+			</div>\
+			</li>');
+
+
+
+
+		function listElements(list){
+			var result = "";
+			list.forEach(function(value){
+				result = result + "<li>" + value + "</li>";
+			});
+			return result;
+		}	
+		
+	}
+	
 }
