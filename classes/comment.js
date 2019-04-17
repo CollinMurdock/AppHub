12e727ef05d5ee9e8608b7eb2e07f46f4b7acb09
CollinMurdock <murdoccr@miamioh.edu>
2019-04-17 11:56:01 -0400

    created comment class

---
+++ classes/comment.js
@@ -0,0 +1,12 @@
+class Comment {
+
+	MAX_TITLE_LENGTH = 75;
+	MAX_BODY_LENGTH = 350;
+	//constructor
+	constructor(username, title, body){
+		this.username = username;
+    	this.title = title;
+		this.body = body;
+  }
+}
+
