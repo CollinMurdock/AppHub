293698ac99728a1341bf265e6a047cd2db80b4c3
CollinMurdock <murdoccr@miamioh.edu>
2019-04-17 11:44:16 -0400

    removed description from main page

--- js/session.js
+++ js/session.js
@@ -76,7 +76,6 @@ class AppItem{
 		this.element = $('<li class="app-item">\
 			<div class="top-block pure-g">\
 				<h1 class="pure-u-1-3 title"><a href="appPage.html?key='+key+'">'+app.name+'</a></h1>\
-				<div class="pure-u-2-3 description" ><p>'+app.desc+'</p></div>\
 			</div>\
 			<div class="bottom-block pure-g">\
 				<div class="pure-u-1-5 version"><p><b>Version</b></p>'+app.version+'</div>\
