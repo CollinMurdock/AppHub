9aa451187724b606f119ede4e068b3c6913369cc
CollinMurdock <murdoccr@miamioh.edu>
2019-04-17 12:44:25 -0400

    some stuff

--- classes/comment.js
+++ classes/comment.js
@@ -7,6 +7,21 @@ class Comment {
 		this.username = username;
     	this.title = title;
 		this.body = body;
-  }
+  	}
+
+  	getHTML(){
+		let element = $('<li class="comment">\
+			<div class="top-block">\
+				<h2 style="display:inline;">'+ this.title +'</h2>\
+				<span style="color:grey;"> from </span>\
+				<span>'+ this.username +'</span>\
+			</div>\
+			<div class="bottom-block">\
+				<p>'+ this.body +'</p>\
+			</div>\
+			</li>');
+
+		return element;
+	}
 }
 
--- js/firebaseFunctions.js
+++ js/firebaseFunctions.js
@@ -125,3 +125,4 @@ function getApp(key){
     });
   });
 }
+
