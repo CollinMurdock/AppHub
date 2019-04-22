78f784dce5154ff3b648ef8d5139e67e1f994283
CollinMurdock <murdoccr@miamioh.edu>
2019-04-22 14:22:03 -0400

    added search button

--- js/session.js
+++ js/session.js
@@ -17,15 +17,15 @@ $(document).ready(function(){
 	let platforms = ["iOS", "Android"];
 
 	//a basic app
-	let app = new App("Facebook",
+	let app = new App("Snapchat",
 					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a eros quis odio scelerisque rhoncus non sed lorem. Nunc tempus augue sed urna egestas, sit amet eleifend ex cursus. Nunc pellentesque eros aliquet nisi hendrerit imperdiet. Aliquam ornare rhoncus volutpat. Duis vitae tellus et purus facilisis blandit. Vestibulum nec ante sed quam dignissim iaculis. Suspendisse in ornare mi, ac mollis orci. Quisque lorem leo, varius non placerat non, interdum non erat. Curabitur congue, sapien ut condimentum facilisis, arcu enim pretium mi, ut faucibus sem neque non tortor. Suspendisse luctus dui eget mi dignissim faucibus. Donec iaculis mi non velit ultricies, dapibus accumsan sem convallis. Ut ac elit mi. Nulla facilisi. Maecenas eu ex maximus, pulvinar nunc sollicitudin, cursus metus.\
 					Cras efficitur, nulla ut luctus ultrices, sapien est egestas turpis, at cursus ante dolor et nulla. Suspendisse vestibulum velit ut ipsum fringilla viverra. Curabitur condimentum, elit mollis iaculis lacinia, risus mauris laoreet nunc, nec rutrum tellus ligula sit amet dui. Maecenas tortor est, semper nec diam a, placerat elementum odio. Quisque magna dolor, rutrum at interdum id, volutpat quis diam. Integer volutpat euismod placerat. Fusce justo quam, fringilla nec interdum non, venenatis id arcu. Aliquam posuere urna eget auctor porta. Fusce.",
-					["Collin", "Jason"],
+					["Span Inc"],
 					platforms,
-					"12.2",
+					"10.2",
 					0,
-					"https://facebook.com",
-					"Status");
+					"https://snapchat.com",
+					"Images");
 	
 	//createApp(app);
 });
