b28f409c07b413679ce7e04668f02d39388c6f6d
Jacob <47573770+Jsf499@users.noreply.github.com>
2019-04-22 21:58:26 -0400

    Update firebaseFunctions.js

--- js/firebaseFunctions.js
+++ js/firebaseFunctions.js
@@ -105,8 +105,9 @@ function onError(error) {
 
 
 function createApp(app){
-  let ref = database.ref('Apps');
-  // where we check to make sure things aren't taken
+	//adds to proposed node
+  let ref = database.ref('ProposedApps');
+  // where we check to make sure things aren't taken (still needs to be done)
   ref.push(app);
 }
 
